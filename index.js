34e48a311e6a4ff59c8dbb4729902ba9e372b641
LupusLudensEst <gurovvic@gmail.com>
2021-10-14 22:06:16 -0400

    dt 14 oct 2021

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const {nanoid} = require('nanoid')
+// const {nanoid} = require('nanoid')
+//
+// console.log(nanoid(), nanoid().length, typeof nanoid());
+//
+// const faker = require('faker');
+// const randomName = faker.name.findName();
+// console.log(randomName, randomName.length, typeof randomName);
 
-console.log(nanoid(), nanoid().length, typeof nanoid());
-
-const faker = require('faker');
-const randomName = faker.name.findName();
-console.log(randomName, randomName.length, typeof randomName);
+const {get} = require('lodash'); // extracts feature from the object and if it does not exist
+// it returns default meaning
+const base = {name: 'Peter', age: 50};
+console.log(base.name);
+console.log(base.age);
+console.log(get(base, 'age.adult', 99));
